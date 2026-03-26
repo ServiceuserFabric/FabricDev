3f1a573c8e2689bb11c9ca419d93381f9267e0d5
ServiceuserFabric <suf@altibox.dk>
2026-03-26 12:44:08 +0100

    Opsætning til LoyaltyGroup

--- Fabric/Rapporteringer/LoyaltyGroup/LoyaltyGroup.Notebook/notebook-content.py
+++ Fabric/Rapporteringer/LoyaltyGroup/LoyaltyGroup.Notebook/notebook-content.py
@@ -65,30 +65,22 @@ import json # To handle exit message for pipeline
 
 
 
-# User defined variables ###########################################################
-# Define your variables here
-dToday = datetime.now()
-dToday = dToday.date() # - timedelta(days=1)
-# For testing, set a manual date
-# dToday = datetime(2026, 3, 21).date()
+# BEMÆRK! AI-genereret kode kan indeholde fejl eller handlinger, du ikke havde til hensigt. Gennemse koden i denne celle omhyggeligt, før du kører den.
 
-# Is monday
-# is_monday = ((dToday + timedelta(days=1)).weekday()) == 0
-# print(is_monday)
+from datetime import datetime
+try:
+    from dateutil.relativedelta import relativedelta
+except ImportError:
+    raise ImportError("Please install python-dateutil for month arithmetic")
 
-# From date. If today is monday, subtract 9 days, else subtract 8 days
-dDate = dToday - timedelta(days=8)
-# dFromDate = dToday - timedelta(days=8) # if is_monday else dToday - timedelta(days=7)
+# Set today's date
+dToday = datetime.now().date()
+# Subtract 3 months, then set day to 1 (beginning of month)
+three_months_ago = dToday - relativedelta(months=2)
+dFromDate = three_months_ago.replace(day=1)
 
-# Subtract 8 days from today and format as date only without time
-# dTodate = dToday - timedelta(days=7)
-# Ensure dFromDate is not equal to dTodate othwerwise SQL query will fail for 'between' statement
-#dFromDate = dFromDate if dFromDate != dTodate else dFromDate - timedelta(days=1)
+print("dFromDate:", dFromDate)
 print("Todays date:", dToday)
-print("Date:", dDate)
-# print("Is it Monday today?:", is_monday)
-# print("From date:", dFromDate)
-# print("To date:", dTodate)
 
 # METADATA ********************
 
@@ -128,7 +120,8 @@ PH.ProduktID,
 PC.selfserviceNames_da,
 C.accounts_address, 
 C.Zip,
-C.City                                        
+C.City,
+C.mobile                                         
 FROM ProductHistory AS PH
 LEFT JOIN Product_config as PC on PC.id = PH.ProduktID
 LEFT JOIN Customer AS C ON C.accounts_id = PH.accountKey                                        
@@ -164,7 +157,8 @@ PH.ProduktID,
 PC.selfserviceNames_da,
 C.accounts_address, 
 C.Zip,
-C.City                                        
+C.City,
+C.mobile                                        
 FROM ProductHistory AS PH
 LEFT JOIN Product_config as PC on PC.id = PH.ProduktID
 LEFT JOIN Customer AS C ON C.accounts_id = PH.accountKey                                        
@@ -212,6 +206,54 @@ print(f"Number of rows: {Golden_Lakehouse_PC.count()}")
 
 # CELL ********************
 
+# Load data ########################################################################
+## Panther: Her henter vi oplysninger om nye kunder, som ikke skal kontaktes pga. KTU
+# Connect to Fabrics sql endpoint 
+
+query_Golden_Lakehouse_NEW = f"""
+SELECT ProductHistory.accountKey, ProductHistory.KundeNO as KundeID,ProductHistory.orderedDate, PC.selfserviceNames_da
+--SELECT *
+FROM ProductHistory
+    INNER JOIN Product_config AS PC
+        ON PC.id = ProductHistory.ProduktID
+    INNER JOIN Customer AS C 
+        ON ProductHistory.accountKey = C.accounts_id
+    INNER JOIN Cust_Group AS GC 
+        ON GC.id = C.customerGroupKey
+WHERE 
+        ProductHistory.KundeNO IS NOT NULL
+    AND ProductHistory.FromDate <= DATE '{dToday.isoformat()}' AND ProductHistory.ToDate > DATE '{dToday.isoformat()}'
+    AND ProductHistory.Kunde_Status = 'Aktive'
+    AND ProductHistory.NySalg = '1'
+    AND ProductHistory.startDate between DATE '{dFromDate.isoformat()}' and DATE '{dToday.isoformat()}'
+        AND (
+            (PC.HovedProduktGruppe = 'Internet' AND PC.productGroup <> 'Migrering')
+            OR
+            (PC.HovedProduktGruppe = 'TV' AND PC.productGroup <> 'Dump')
+        )
+        AND (
+            GC.fullPath LIKE '%Erhverv%'
+            OR GC.fullPath LIKE '%Privat%'
+            OR GC.fullPath LIKE '%Medarbejderbredbånd%'
+        ) AND ca_created = ''
+"""
+
+
+Golden_Lakehouse_NEW = spark.sql(query_Golden_Lakehouse_NEW)
+Golden_Lakehouse_NEW.show(10, truncate=False)
+print(f"Number of rows: {Golden_Lakehouse_NEW.count()}")
+
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
 # Her skifter vi til at hente data fra bronze
 # set schema 
 spark.catalog.setCurrentDatabase("Bronze_lakehouse")
@@ -235,7 +277,7 @@ spark.sql("SHOW TABLES").show(truncate=False)
 query_Bronze_Lakehouse_Email = f"""
 SELECT 
 C.accounts_id, 
-C.accounts_email,
+C.email,
 CG.name as customerGroupName, 
 C.customerGroupKey, 
 CG.fullPath                                         
@@ -318,9 +360,30 @@ print(f"Number of rows: {Bronze_Lakehouse_ProvModel.count()}")
 
 # Henter informatioer på mobilkunder
 # set connection to golden warehouse
-df = spark.read.synapsesql(
-    "Golden_warehouse.nuu.AltiboxMobil_Nuuday"
+# Connecting to a warehouse requires another specific package 
+import com.microsoft.spark.fabric
+from com.microsoft.spark.fabric.Constants import Constants
+
+AltiboxMobil_Nuuday = (
+    spark.read
+        .option(Constants.DatabaseName, "Golden_warehouse")
+        .synapsesql(
+            """
+            SELECT
+                [Mobil nummer],
+                [Kunde Navn],
+                [E-Mail],
+                [Kunde Adresse],
+                [Kunde Postnummer],
+                [Kunde By]
+            FROM nuu.AltiboxMobil_Nuuday
+            WHERE LastVersion = 1
+              AND Opsigelsesdato IS NOT NULL
+            """
+        )
 )
+
+display(AltiboxMobil_Nuuday)
 
 # METADATA ********************
 
@@ -403,8 +466,13 @@ Golden_Lakehouse_Products = Golden_Lakehouse_Products.filter(F.col("KundeNO") !=
 Golden_Lakehouse_Products = Golden_Lakehouse_Products.filter(F.col("KundeNO").isNotNull())
 # Delete duplicate rows
 Golden_Lakehouse_Products = Golden_Lakehouse_Products.dropDuplicates()
+# Coalesce mobile_Internet and mobile_TV
+Golden_Lakehouse_Products = Golden_Lakehouse_Products.withColumn(
+    "mobile",
+    F.coalesce(F.col("mobile_Internet"), F.col("mobile_TV"))
+)
 # Order by accountKey_Internet
-Golden_Lakehouse_Products.show(50, truncate=False)
+Golden_Lakehouse_Products.show(10, truncate=False)
 
 print(f"Number of rows: {Golden_Lakehouse_Products.count()}")
 
@@ -424,7 +492,7 @@ print(f"Number of rows: {Golden_Lakehouse_Products.count()}")
 # Select only what you need from email table to keep it clean
 email_sel = Bronze_Lakehouse_Email.select(
     F.col("accounts_id").alias("accounts_id"),
-    F.col("accounts_email").alias("accounts_email"),
+    F.col("email").alias("email"),
     F.col("customerGroupName").alias("customerGroupName"),
     F.col("customerGroupKey").alias("customerGroupKey"),
     F.col("fullPath").alias("fullPath")
@@ -432,14 +500,14 @@ email_sel = Bronze_Lakehouse_Email.select(
 
 email_i = (email_sel
            .withColumnRenamed("accounts_id", "accounts_id_i")
-           .withColumnRenamed("accounts_email", "accounts_email_i")
+           .withColumnRenamed("email", "email_i")
            .withColumnRenamed("customerGroupName", "customerGroupName_i")
            .withColumnRenamed("customerGroupKey", "customerGroupKey_i")
            .withColumnRenamed("fullPath", "fullPath_i"))
 
 email_t = (email_sel
            .withColumnRenamed("accounts_id", "accounts_id_t")
-           .withColumnRenamed("accounts_email", "accounts_email_t")
+           .withColumnRenamed("email", "email_t")
            .withColumnRenamed("customerGroupName", "customerGroupName_t")
            .withColumnRenamed("customerGroupKey", "customerGroupKey_t")
            .withColumnRenamed("fullPath", "fullPath_t"))
@@ -451,7 +519,7 @@ Merge_Golden_Bronze = (Golden_Lakehouse_Products
 
 # Coalesce fields
 Merge_Golden_Bronze = (Merge_Golden_Bronze
-    .withColumn("Email", F.coalesce(F.col("accounts_email_i"), F.col("accounts_email_t")))
+    .withColumn("Email", F.coalesce(F.col("email_i"), F.col("email_t")))
     .withColumn("name",  F.coalesce(F.col("name_TV"), F.col("name_Internet")))
     .withColumn("customerGroupName", F.coalesce(F.col("customerGroupName_t"), F.col("customerGroupName_i")))
     .withColumn("customerGroupKey",  F.coalesce(F.col("customerGroupKey_i"),  F.col("customerGroupKey_t")))
@@ -461,6 +529,82 @@ Merge_Golden_Bronze = (Merge_Golden_Bronze
 
 Merge_Golden_Bronze.show(20, truncate=False)
 print(f"Number of rows:{Merge_Golden_Bronze.count()}")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+# # -----------------------------
+# 2.1 Left join Golden_Lakehouse_Products with informations on ALtiboxMobil_Nuuday
+# ----------------------------
+
+# Check format from AltiboxMobil_Nuuday.view
+AltiboxMobil_Nuuday.show(3, truncate=False)
+Merge_Golden_Bronze.show(3, truncate=False)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+# # -----------------------------
+# 2.1 Left join Golden_Lakehouse_Products with informations on ALtiboxMobil_Nuuday
+# ----------------------------
+
+# Check format from AltiboxMobil_Nuuday.view
+AltiboxMobil_Nuuday.show(3, truncate=False)
+Merge_Golden_Bronze.show(3, truncate=False)
+
+# Add column in Golden_Lakehouse_Products. It is set to 0 or 1. 0 as default and 1 if there is a match 
+# when joining Golden_Lakehouse_Products['mobile', Email] with AltiboxMobil_Nuuday['Mobil nummer', 'E-mail']
+from pyspark.sql import functions as F
+
+AltiboxMobil_Nuuday = AltiboxMobil_Nuuday.withColumnRenamed("Mobil nummer", "mobile_number") \
+                                       .withColumnRenamed("E-Mail", "email_mobil")
+
+Merge_Golden_Bronze = Merge_Golden_Bronze.withColumn("Produkttype_Mobil", F.lit(0))
+
+Merge_Golden_Bronze = Merge_Golden_Bronze.join(
+    AltiboxMobil_Nuuday,
+    (F.col("mobile") == F.col("mobile_number")) | (F.col("Email") == F.col("email_mobil")),
+    "left")
+#Merge_Golden_Bronze = Merge_Golden_Bronze.join(
+#    AltiboxMobil_Nuuday,
+#    (F.col("mobile") == F.col("mobile_number")),
+#    "left")
+Merge_Golden_Bronze = Merge_Golden_Bronze.withColumn(
+    "Produkttype_Mobil",
+    F.when(F.col("mobile_number").isNotNull() | F.col("email_mobil").isNotNull(), 1).otherwise(0)
+)
+
+Merge_Golden_Bronze.show(10, truncate=False)
+print(f"Number of rows: {Merge_Golden_Bronze.count()}")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+# Filter and show rows where is_mobile_customer == 1
+filtered = Merge_Golden_Bronze.filter(F.col("Produkttype_Mobil") == 1)
+filtered.show(10, truncate=False)  # Show the matching rows
+
+# Print the number of rows in the filtered result
+row_count = filtered.count()
+print(f"Number of rows: {row_count}")
 
 # METADATA ********************
 
@@ -483,13 +627,14 @@ df_Loyalty = Merge_Golden_Bronze.select(
     "ProduktID_Internet",
     "ProduktID_TV",
     "selfserviceNames_da_TV",
+    "Produkttype_Mobil", 
     "Zip",
     "City",
     "customerGroupName",
     "fullPath"
 )
 
-df_Loyalty.show(100, truncate=False)
+df_Loyalty.show(10, truncate=False)
 
 # METADATA ********************
 
@@ -627,22 +772,6 @@ df_Loyalty = df_Loyalty.withColumn(
 # CELL ********************
 
 # -----------------------------
-# 9) Add Produkttype_Mobil = null
-# -----------------------------
-df_Loyalty = df_Loyalty.withColumn("Produkttype_Mobil", F.lit(None).cast("string"))
-
-df_Loyalty.show(100, truncate=False)
-
-# METADATA ********************
-
-# META {
-# META   "language": "python",
-# META   "language_group": "synapse_pyspark"
-# META }
-
-# CELL ********************
-
-# -----------------------------
 # 10) Select output columns in order + rename
 # -----------------------------
 df_Loyalty = df_Loyalty.select(
@@ -705,7 +834,7 @@ df_Loyalty = df_Loyalty.withColumn(
 # 12) Anti-join: remove customers already in previous sample
 # -----------------------------
 df_Loyalty = df_Loyalty.join(
-    Rapporteringer_LoyaltyGroup_Sample.select("KundeID").distinct(),
+    df_rapporteringer.select("KundeID").distinct(),
     on="KundeID",
     how="left_anti"
 )
@@ -722,11 +851,36 @@ df_Loyalty = df_Loyalty.join(
 # CELL ********************
 
 # -----------------------------
-# 13) Weighted sample of 6000 unique KundeID
+# 13) Anti-join: remove customers who started with Altibox within last two months, as they received KTU
+# -----------------------------
+
+df_Loyalty = df_Loyalty.join(
+    Golden_Lakehouse_NEW.select("KundeID").distinct(),
+    on="KundeID",
+    how="left_anti"
+)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+# -----------------------------
+# 14) Weighted sample of 6000 unique KundeID
 #     Weight = 0.25 for customers with BOTH internet speed and TV, else 1.
 #     Use exponential-race key to do weighted sampling without replacement.
 # -----------------------------
 df_Loyalty_unique = df_Loyalty.dropDuplicates(["KundeID"])
+df_Loyalty_unique = (
+    df_Loyalty_unique
+        .withColumn("Email_norm", F.lower(F.col("Email")))
+        .dropDuplicates(["Email_norm"])
+        .drop("Email_norm")
+)
 
 weight_col = F.when(
     F.col("Produkttype_Speed").isNotNull() & F.col("Produkttype_TV").isNotNull(),
@@ -741,7 +895,7 @@ key = (-F.log(u)) / weight_col
 df_sample = (df_Loyalty_unique
     .withColumn("_wkey", key)
     .orderBy(F.col("_wkey").asc())
-    .limit(6000)
+    .limit(12000)
     .drop("_wkey")
 )
 
@@ -757,16 +911,109 @@ df_sample = (df_Loyalty_unique
 # CELL ********************
 
 # -----------------------------
-# 14) Add todays date (dToday) into Udsendelsesdato
+# 15) Add todays date (dToday) into Udsendelsesdato
 # -----------------------------
-df_sample_table = df_sample.withColumn("Udsendelsesdato", F.lit(dToday))# -----------------------------
-# 14) Add todays date (dToday) into Udsendelsesdato
-# -----------------------------
-df_sample_table = df_sample.withColumn("Udsendelsesdato", F.lit(dToday))
+df_sample_table_date = df_sample.withColumn("Udsendelsesdato", F.lit(dToday))
+df_sample_table_date.show(10, truncate=False)
 
 # METADATA ********************
 
 # META {
 # META   "language": "python",
 # META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+# Save to table in SQL
+rapporteringer_path = (
+    "abfss://6b06974a-4346-4a38-bc5a-d42e564a6bec@onelake.dfs.fabric.microsoft.com/8c6daa7e-25e3-49f4-8f00-7ba8daf52bc3/Tables/dbo/LoyaltyGroup"
+)
+df_sample_table_date.write.mode("append").save(rapporteringer_path)
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark",
+# META   "frozen": false,
+# META   "editable": true
+# META }
+
+# CELL ********************
+
+# Save to csv in files by naming it with variable dToday
+#df_sample.write.csv(rapporteringer_path, header=True)
+
+#df_sample = write_single_csv(df_sample, rapporteringer_path, "Emner.csv")
+from pyspark.sql import functions as F
+from notebookutils import mssparkutils
+import re
+# Save to csv in files by naming it with variable dToday
+rapporteringer_path = (
+    f"abfss://6b06974a-4346-4a38-bc5a-d42e564a6bec@onelake.dfs.fabric.microsoft.com/"
+    f"8c6daa7e-25e3-49f4-8f00-7ba8daf52bc3/Files/LoyaltyGroup/{dToday}_LoyaltyGroup"
+)
+
+# Write a single consolidated CSV file (non-splittable)
+def write_single_csv(df, output_path, filename):
+    # Repartition to 1 partition to ensure a single output file
+    temp_path = output_path + "_tmp"
+    (
+        df.repartition(1)
+        .write
+        .option("header", True)
+        .mode("overwrite")
+        .csv(temp_path)
+    )
+    import re
+    from notebookutils import fs
+    # Find part file (use attribute access .path)
+    files = fs.ls(temp_path)
+    part_file = next((f.path for f in files if re.match(r".*/part-.*\.csv$", f.path)), None)
+    if part_file is None:
+        raise FileNotFoundError("Could not find the single output part-*.csv file.")
+    target = output_path + "/" + filename
+    fs.cp(part_file, target)
+    fs.rm(temp_path, recurse=True)
+    return df
+
+# Call function to write single CSV file
+write_single_csv(df_sample, rapporteringer_path, "Emner.csv")
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+# Used for one time case to add data from one table into another
+from pyspark.sql import functions as F
+
+# Read source table
+df_src = spark.read.table("Rapporteringer.LoyaltyGroup_old")
+
+# Change / override a column before append
+df_transformed = df_src.withColumn(
+    "source_system",
+    F.lit("CRM")         
+)
+
+# Append into target table
+df_transformed.write \
+    .mode("append") \
+    .saveAsTable("Rapporteringer.TargetTable")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark",
+# META   "frozen": true,
+# META   "editable": false
 # META }
