e61e91ec3b71d953737b80c68c4e457dd3825536
ServiceuserFabric <suf@altibox.dk>
2026-09-25 14:20:54 +0200

    Committing 1 item from workspace 6b06974a-4346-4a38-bc5a-d42e564a6bec

---
+++ Fabric/Microsoft AD/Drop AD tables.Notebook/notebook-content.py
@@ -0,0 +1,36 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "9b2f31d6-7763-4e3f-a1dd-929063e067b3",
+# META       "default_lakehouse_name": "MicrosoftAD",
+# META       "default_lakehouse_workspace_id": "6b06974a-4346-4a38-bc5a-d42e564a6bec",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "9b2f31d6-7763-4e3f-a1dd-929063e067b3"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+spark.sql('DROP TABLE MicrosoftAD.MicrosoftAD_group_memberships')
+spark.sql('DROP TABLE MicrosoftAD.MicrosoftAD_groups')
+spark.sql('DROP TABLE MicrosoftAD.MicrosoftAD_users')
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
